08e973cb2f9e9435140a7d0382f63342b618f18c
Aleksandar Vrencev <aleksandarvrencev@gmail.com>
2022-07-04 18:57:01 +0200

    fixed formating,added presentation file

--- part_manager_sindikalna.py
+++ part_manager_sindikalna.py
@@ -1,12 +1,17 @@
+# imports
 from tkinter import *
 from tkinter import messagebox
 import re
 from db_sindikalna import Database
 from datetime import *
 from dateutil.relativedelta import relativedelta
+import tkinter.font as tkFont
+
+# initialize database
 db_sindikalna = Database('sindikalna.db')
 
-def IzborBrojaRata():
+# functions
+def set_number_of_rates():
     regex_check()
     pojedinacna_rata_text.set(int((ukupan_iznos_text.get() - uplaceno_text.get()) / broj_rata_text.get()))
 
@@ -14,39 +19,39 @@ def regex_check():
     re_sindikat_entry = re.match("^[A-Z][-a-zA-Z]+$", sindikat_entry.get())
     # if not re_sindikat_entry:
     if sindikat_entry.get().__len__() < 3 or sindikat_entry.get().__len__() > 30:
-        messagebox.showinfo(message="Polje 'Sindikat' je prazno ili nije odgovarajuce duzine")
+        messagebox.showinfo(message="Polje 'Sindikat' je prazno ili nije odgovarajuće dužine.")
         return False
     re_ime_entry = re.match("^[A-Z][-a-zA-Z]+$", ime_entry.get())
     # if not re_ime_entry:
     if ime_entry.get().__len__() < 3 or ime_entry.get().__len__() > 30:
-        messagebox.showinfo(message="Polje 'Ime' je prazno,pocinje brojem ili nije odgovarajuce duzine")
+        messagebox.showinfo(message="Polje 'Ime' je prazno,počinje brojem ili nije odgovarajuće dužine.")
         return False
     re_prezime_entry = re.match("^[A-Z][-a-zA-Z]+$", prezime_entry.get())
     # if not re_prezime_entry:
     if ime_entry.get().__len__() < 3 or ime_entry.get().__len__() > 30:
-        messagebox.showinfo(message="Polje 'Prezime' je prazno,pocinje brojem ili nije odgovarajuce duzine")
+        messagebox.showinfo(message="Polje 'Prezime' je prazno,počinje brojem ili nije odgovarajuće dužine.")
         return False
     re_ukupan_iznos_entry = re.match("^[1-9]+[0-9]*$", ukupan_iznos_entry.get())
     if not re_ukupan_iznos_entry:
-        messagebox.showinfo(message="Polje 'Ukupan iznos' nije popunjeno ili ne sadrzi iskljucivo pozitivne brojeve")
+        messagebox.showinfo(message="Polje 'Ukupan iznos' nije popunjeno ili ne sadrži isključivo pozitivne brojeve.")
         return False
     re_uplaceno_entry = re.match(r"[0-9]{1,7}", uplaceno_entry.get())
     if not re_uplaceno_entry:
-        messagebox.showinfo(message="Polje 'Uplaceno pri kupovini' nije popunjeno ili ne sadrzi iskljucivo pozitivne brojeve")
+        messagebox.showinfo(message="Polje 'Uplaćeno pri kupovini' nije popunjeno ili ne sadrži isključivo pozitivne brojeve.")
         return False
     re_datum_prve_rate_entry = re.match(r"^20[0-2][0-9]-((0[1-9])|(1[0-2]))-(0[1-9]|[1-2][0-9]|3[0-1])$", datum_prve_rate_entry.get())
     if not re_datum_prve_rate_entry:
-        messagebox.showinfo(message="Polje 'Datum prve rate' nije popunjeno ili nije upisano u ispravnom fromatu.Primer 2022-06-24")
+        messagebox.showinfo(message="Polje 'Datum prve rate' nije popunjeno ili nije upisano u ispravnom formatu.Primer 2022-06-24.")
         return False
     re_broj_rata_entry = re.match("^(1[0-2]|[1-9])$", broj_rata_entry.get())
     if not re_broj_rata_entry:
-        messagebox.showinfo(message="Polje 'Broj rata' nije popunjeno ili nije u opsegu 1-12")
+        messagebox.showinfo(message="Polje 'Broj rata' nije popunjeno ili nije u opsegu 1-12.")
         return False
     # if int(broj_rata_entry.get()) * int(float(pojedinacna_rata_entry.get())) != int(ukupan_iznos_entry.get()) - int(uplaceno_entry.get()):
     #     messagebox.showinfo(message="Broj rata pomnozen sa iznosom pojedinacnih rata nije jednak ukupnom iznosu za placanje umanjenom za iznos uplacen pri kupovini")
     #     return False
     if int(ukupan_iznos_entry.get()) < int(uplaceno_entry.get()):
-        messagebox.showinfo(message="Iznos uplacen pri kupovini ne moze biti veci od ukupnog iznosa")
+        messagebox.showinfo(message="Iznos uplacen pri kupovini ne moze biti veci od ukupnog iznosa.")
         return False
     return True
 
@@ -55,7 +60,12 @@ def populate_list():
     counter = 0
     parts_list.delete(0, END)
     for row in db_sindikalna.fetch():
-        parts_list.insert(END, "ID:"+str(row[0])+" Sindikat:"+row[1]+" Ime:"+row[2]+" Prezime:"+ str(row[3])+ " Ukupno:"+str(row[4])+" Uplaceno pri kupovini"+str(row[5])+" Br.rata:"+str(row[6])+" Iznos svake rate:"+str(row[7]))
+        brojac_sindikat=40
+        brojac_ime=40
+        brojac_prezime=40
+        # parts_list.insert(END, "ID:"+str(row[0])+" Sindikat:"+row[1]+" Ime:"+row[2]+" Prezime:"+ str(row[3])+ " Ukupno:"+str(row[4])+" Uplaceno pri kupovini"+str(row[5])+" Br.rata:"+str(row[6])+" Iznos svake rate:"+str(row[7]))
+        # parts_list.insert(END, "ID:  "+str(row[0])+"  Sindikat: "+row[1]+' '*(30-str(row[1]).__len__())+"Ime:"+row[2]+' '*(30-str(row[2]).__len__())+"Prezime:"+ str(row[3])+' '*(30-str(row[3]).__len__()))
+        parts_list.insert(END, "{:>3}{:<5}".format("ID:",str(row[0]))+"{:>12}{:<30}".format("Sindikat: ",row[1])+"{:>5}{:<30}".format("ime",row[2])+ "{:>10}{:<30}".format("Prezime:",str(row[3])))
 
 def populate_list_date():
     global counter
@@ -110,23 +120,12 @@ def select_item(event):
         pojedinacna_rata_entry.insert(END, selected_item[7])
         datum_prve_rate_entry.delete(0, END)
         datum_prve_rate_entry.insert(END, selected_item[8])
-
     except IndexError:
         pass
 
-def create_rate(i,text_rate='\t',int_rate='\t'):
-    label_string_var = StringVar()
-    label_string_var.set(text_rate)
-    label_int_var = StringVar()
-    label_int_var.set(int_rate)
-    rata_label = Label(app, textvariable=label_string_var)
-    rata_label.grid(row=14, column=i, padx=15)
-    iznos_label = Label(app, textvariable=label_int_var)
-    iznos_label.grid(row=18, column=i, padx=15)
-
 def remove_item():
-    brisanje_bool = messagebox.askyesno(message="Da li zaista želite da obrišete izabrani podatak?")
-    if brisanje_bool:
+    remove_boolean = messagebox.askyesno(message="Da li zaista želite da obrišete izabrani podatak?")
+    if remove_boolean:
         db_sindikalna.remove(selected_item[0])
         clear_text()
         populate_list()
@@ -150,59 +149,61 @@ def clear_text():
 # Create window object
 app = Tk()
 
-# Sindikat
+# Labels and Entry fields
+# Union
 sindikat_text = StringVar()
 sindikat_label = Label(app, text='Sindikat', font=('bold', 14))
 sindikat_label.grid(row=2, column=0, sticky=W, pady=(100,10), padx=(50,5))
 sindikat_entry = Entry(app, textvariable=sindikat_text, width=20)
 sindikat_entry.grid(row=2, column=1, pady=(100,10), padx=30)
-# Ime
+# First name
 ime_text = StringVar()
 ime_label = Label(app, text='Ime', font=('bold', 14))
 ime_label.grid(row=3, column=0, sticky=W, pady=(0,10), padx=(50,5))
 ime_entry = Entry(app, textvariable=ime_text)
 ime_entry.grid(row=3, column=1, pady=(0,10), padx=30)
-# Prezime
+# Last name
 prezime_text = StringVar()
 prezime_label = Label(app, text='Prezime', font=('bold', 14))
 prezime_label.grid(row=4, column=0, sticky=W, pady=(0,10), padx=(50,5))
 prezime_entry = Entry(app, textvariable=prezime_text)
 prezime_entry.grid(row=4, column=1, pady=(0,10), padx=30)
-# Ukupan iznos
+# Total amount
 ukupan_iznos_text = IntVar()
 ukupan_iznos_label = Label(app, text='Ukupan iznos', font=('bold', 14))
 ukupan_iznos_label.grid(row=5, column=0, sticky=W, pady=(0,10), padx=(50,5))
 ukupan_iznos_entry = Entry(app, textvariable=ukupan_iznos_text)
 ukupan_iznos_entry.grid(row=5, column=1, pady=(0,10), padx=30)
-# labela i entry za iznos koji je uplacen pri kupovini
+# Label and Entry for the amount paid at the time of purchase
 uplaceno_label = Label(app, text='Uplaćeno pri kupovini', font=('bold', 14))
 uplaceno_label.grid(row=6, column=0, sticky=W, pady=(0,10), padx=(50,5))
 uplaceno_text = IntVar()
 uplaceno_entry = Entry(app, textvariable=uplaceno_text)
 uplaceno_entry.grid(row=6, column=1, pady=(0,10), padx=30)
-# Datum prve rate
+# Date for first rate
 datum_prve_rate_text = StringVar()
 datum_prve_rate_label = Label(app, text='Datum prve rate(format:yyyy-mm-dd)', font=('bold', 14))
 datum_prve_rate_label.grid(row=7, column=0, sticky=W, pady=(0,10), padx=(50,5))
 datum_prve_rate_entry = Entry(app, textvariable=datum_prve_rate_text)
 datum_prve_rate_entry.grid(row=7, column=1, pady=(0,10), padx=50)
-# Broj rata
+# Number of rates
 BROJEVI = list(range(1,13))
 broj_rata_text = IntVar()
 broj_rata_label = Label(app, text='Broj rata', font=('bold', 14))
 broj_rata_label.grid(row=8, column=0, sticky=W, pady=(0,10), padx=(50,5))
 broj_rata_entry = Entry(app, textvariable=broj_rata_text)
 broj_rata_entry.grid(row=8, column=1, sticky=W, pady=(0,10), padx=(50,5))
-izracunaj_rate_btn = Button(app, text='Izračunaj', width=12, command=IzborBrojaRata)
+izracunaj_rate_btn = Button(app, text='Izračunaj', width=12, command=set_number_of_rates)
 izracunaj_rate_btn.grid(row=10, column=1)
-# labela i entry za iznos pojedinacnih rata
+# Label and Entry for value of single rate
 pojedinacna_rata_label = Label(app, text='Iznos pojedinačnih rata', font=('bold', 14))
 pojedinacna_rata_label.grid(row=9, column=0, sticky=W, pady=(0,10), padx=(50,5))
 pojedinacna_rata_text = IntVar()
 pojedinacna_rata_entry = Entry(app, textvariable=pojedinacna_rata_text)
 pojedinacna_rata_entry.grid(row=9, column=1, pady=(0,10), padx=30)
 # Parts List (Listbox)
-parts_list = Listbox(app, height=15, width=100, border=0)
+# small_font = tkFont.Font(size=10)
+parts_list = Listbox(app, height=15, width=120, border=0)
 parts_list.grid(row=2, column=5, columnspan=6, rowspan=6, pady=(100,0), padx=(0,0))
 # Create scrollbar
 scrollbar = Scrollbar(app, orient='vertical')
@@ -232,7 +233,7 @@ show_btn = Button(app, text='Svi podaci', width=12, command=populate_list)
 show_btn.grid(row=10, column=10)
 
 app.title('Sindikalna prodaja')
-app.geometry('1150x500')
+app.geometry('1250x500')
 
 # Populate data
 populate_list()
